4e2f7bb633983c1f058dc91bfffe69a82d6240c4
wjh123 <894275157@qq.com>
2020-01-15 22:22:22 +0800

    支付版本1，组件staggeredpPlusMinusChart初始化（橙色）

---
+++ src/components/StaggeredpPlusMinusChart.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="staggeredp-plus-minus-chart" :style="{color: colorFont}">
+        <div class="background-line">
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+
+            <div class="decloration-line" :style="{backgroundColor: colorA}" style="width: 2px"></div>
+
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+            <div class="decloration-line"></div>
+        </div>
+        <div class="line" v-for="(li, index) in chartConfig" :key="index">
+            <div style="width: 24px"><p style="float: right;width: max-content">{{li.name[0]}}</p></div>
+            <div class="chart-bar">
+                <div class="container">
+                    <div :data-number="(li.percent / li.axis_max) * 100 + '%'" class="left-bar" :style="{width: (li.percent / li.axis_max) * 100 + '%', backgroundColor: (li.percent / li.axis_max) * 100 >= 50 ? colorA : colorB}"></div>
+                </div>
+                <div class="container">
+                    <div :data-number="parseInt(100 - ((li.percent / li.axis_max) * 100)) + '%'" class="right-bar" :style="{width: parseInt(100 - ((li.percent / li.axis_max) * 100)) + '%', backgroundColor: (li.percent / li.axis_max) * 100 < 50 ? colorA : colorB}"></div>
+                </div>
+            </div>
+            <div style="width: 24px"><p style="float: left;width: max-content">{{li.name[1]}}</p></div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import {Component, Vue, Prop} from 'vue-property-decorator'
+    interface StaggeredPlusMinusChartConfig {
+        name: [string, string],
+        percent: number,
+        axis_max: number // 最大值
+    }
+
+    @Component({
+        components: {}
+    })
+    export default class StaggeredpPlusMinusChart extends Vue {
+        @Prop(String) private colorA: string | undefined
+        @Prop(String) private colorB: string | undefined
+        @Prop(String) private colorFont: string| undefined
+        private chartConfig: StaggeredPlusMinusChartConfig[] = []
+
+        // 改变position为可用的数据
+        private convertPositionServeData(position: any) { // 急 类型还没空补全
+            for (const i in position) {
+                this.chartConfig.push({
+                    name: [position[i].dimension, position[i].title],
+                    percent: position[i].score,
+                    axis_max: position[i].axis_max
+                })
+            }
+            console.log(
+                this.chartConfig
+            )
+        }
+        // protected render() {
+        //     return <div>123</div>
+        // }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .less-color {
+        background-color: #FFE8D6 !important;
+    }
+
+    .background-line {
+        // overflow: hidden;
+        // background-color: rgba(0, 0, 0, 0.5);
+        // z-index: -1;
+        position: absolute;
+        width: 178px;
+        height: calc(100% + 38px);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        right: 50%;
+        top: 50%;
+        transform: translateX(50%) translateY(-50%);
+        .center-color {
+            background: linear-gradient(#FFFFFF, #FFE8D6, #FFE8D6, #FFFFFF) !important;
+        }
+        .decloration-line {
+            // margin-left: 10px;
+            // margin-right: 10px;
+            height: 100%;
+            width: 1px;
+            // position: relative;
+            // right: -2px;
+            background: linear-gradient(#FFFFFF, #F7F7F7, #F7F7F7, #FFFFFF);
+        }
+    }
+
+    .staggeredp-plus-minus-chart {
+        width: 301px;
+        font-size: 11px;
+        color: #5C5149;
+        // overflow: hidden;
+        position: relative;
+        .line {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-top: 19px;
+            margin-bottom: 19px;
+        }
+        .chart-bar {
+            width: 178px;
+            display: flex;
+            height: 18px;
+            justify-content: center;
+            align-items: baseline;
+            .container {
+                height: 18px;
+                width: 50%;
+            }
+            .left-bar {
+                float: right;
+            }
+            .left-bar, .right-bar {
+                height: 18px;
+                background-color: #FFB074;
+            }
+            .left-bar {
+                border-bottom-left-radius: 2px;
+                border-top-left-radius: 2px;
+                position: relative;
+            }
+            .left-bar::after {
+                position: absolute;
+                left: 0px;
+                top: 50%;
+                transform: translateX(-100%) translateY(-50%);
+                content: attr(data-number);
+            }
+            .right-bar {
+                position: relative;
+                width: unset;
+                border-bottom-right-radius: 2px;
+                border-top-right-radius: 2px;
+            }
+            .right-bar::before {
+                position: absolute;
+                right: 0px;
+                top: 50%;
+                transform: translateX(100%) translateY(-50%);
+                content: attr(data-number);
+            }
+        }
+    }
+</style>
--- src/unit/wjhJS.js
+++ src/unit/wjhJS.js
@@ -156,4 +156,27 @@ export default class wjhTool {
     }
     return sortObj
   }
+
+  static funcUrlDel (name) {
+    let loca = window.location
+    let baseUrl = loca.origin + loca.pathname + '?'
+    let query = loca.search.substr(1)
+    if (query.indexOf(name)>-1) {
+      let obj = {}
+      let arr = query.split('&')
+      for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].split('=')
+        obj[arr[i][0]] = arr[i][1];
+      }
+      delete obj[name]
+      let url = baseUrl + JSON.stringify(obj).replace(/[\"\{\}]/g,'').replace(/\:/g,'=').replace(/\,/g,'&')
+      return url
+    }
+  }
+
+  static changeUrlArg(url, arg, val){
+    let pattern = arg+'=([^&]*)'
+    let replaceText = arg+'='+val
+    return url.match(pattern) ? url.replace(eval('/('+ arg+'=)([^&]*)/gi'), replaceText) : (url.match('[\?]') ? url+'&'+replaceText : url+'?'+replaceText)
+  }
 }
